2392350cacc631d01f7bbd373bb79a3d223fd2f9
mmarcoschambi <marcos02ch@gmail.com>
2023-06-30 05:49:45 -0300

    Actualizacion del controlador de testimonios
    -Se agregó la anotación @Autowired para inyectar la dependencia de ITestimonioService
    -Nuevos métodos nuevoTestimonio y guardarTestimonio.

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/TestimonioController.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/TestimonioController.java
@@ -1,16 +1,33 @@
 package ar.edu.unju.fi.controller;
 
 
+import ar.edu.unju.fi.entity.Testimonio;
+import ar.edu.unju.fi.service.ITestimonioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/testimonios")
 public class TestimonioController {
+	@Autowired
+	private ITestimonioService testimonioServicio;
 	@GetMapping("/todos")
-	public String getRecetas(Model model) {
+	public String getTestimonios(Model model) {
 		return "testimonios";
+	}
+	@GetMapping("/nuevo_testimonio")
+	public String nuevaReceta(Model model) {
+		model.addAttribute("Testimonio", testimonioServicio.getTestimonio());
+		return "nuevo_testimonio";
+	}
+	@PostMapping("/guardar_testimonio")
+	public String guardarReceta(@ModelAttribute("Testimonio") Testimonio testimonio, Model model) {
+		testimonioServicio.guardarTestimonio(testimonio);
+		return "redirect:/testimonios/todos";
 	}
 
 }
