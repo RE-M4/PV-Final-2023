c8ec3f51ad34ee83b1951f06cb1e7647a30d5c0f
mmarcoschambi <marcos02ch@gmail.com>
2023-06-29 22:49:26 -0300

    - Implementación de la interfaz IIngredienteService - Implementación de la interfaz ITestimonioRepository

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/repository/ITestimonioRepository.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/repository/ITestimonioRepository.java
@@ -1,5 +1,11 @@
 package ar.edu.unju.fi.repository;
 
-public interface ITestimonioRepository {
+import ar.edu.unju.fi.entity.Testimonio;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITestimonioRepository extends CrudRepository<Testimonio, Long> {
+	public Testimonio findByCodigo(String codigo);
 
 }
