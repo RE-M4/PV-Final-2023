12af2f3a27af8c95c064ebdf20955caf501c8a90
Mauro Levi Rendón <88637963+RE-M4@users.noreply.github.com>
2023-06-30 18:56:33 -0300

    Actualización del controlador Recetas
    -Se agrega peticiones para guardar y modificar.

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/RecetasController.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/RecetasController.java
@@ -14,25 +14,39 @@ import ar.edu.unju.fi.service.IRecetaService;
 @RequestMapping("/recetas")
 public class RecetasController {
 	
+	private Boolean edicion = false;
+	
 	@Autowired
 	private IRecetaService recetaServicio;
 	@Autowired
 	private IIngredienteService ingredienteServicio;
 	
 	@GetMapping("/todos")
-	public String getRecetas(Model model) {
+	public String getPaginaRecetas(Model model) {
 		return "recetas";
 	}
+	@GetMapping("/ver_recetas/{categoria}")
+	public String getVerRecetas(@PathVariable(value="categoria")String categoria, Model model) {
+		return null;
+	}
 	@GetMapping("/nueva_receta")
-	public String nuevaReceta(Model model) {
+	public String getNuevaReceta(Model model) {
+		edicion = false;
 		model.addAttribute("Receta",recetaServicio.getReceta());
 		model.addAttribute("Ingredientes",ingredienteServicio.getListaIngredientes());
+		model.addAttribute("edicion",edicion);
 		return "nueva_receta";
 	}
 	@PostMapping("/guardar_receta")
-	public String guardarReceta(@ModelAttribute("Receta")Receta receta,Model model) {
-		System.out.println(receta);
+	public String postGuardarReceta(@ModelAttribute("Receta")Receta receta,Model model) {
 		recetaServicio.guardarReceta(receta);
+		//model.addAttribute("")
 		return "redirect:/recetas/todos";
+	}
+	@GetMapping("/modificar_receta/{id}")
+	public String getModificarReceta(@PathVariable(value="id")Long id, Model model) {
+		edicion = true;
+		model.addAttribute("Receta",recetaServicio.getById(id));
+		return null;
 	}
 }
