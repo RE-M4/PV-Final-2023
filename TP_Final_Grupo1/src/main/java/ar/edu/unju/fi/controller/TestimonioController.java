ca00ac306602fb3c126308ae6000960e535aa7d4
mmarcoschambi <marcos02ch@gmail.com>
2023-06-30 02:43:28 -0300

    -Se agrega un GetMapping para retornar a testimonios.html

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/TestimonioController.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/TestimonioController.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.fi.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/testimonios")
 public class TestimonioController {
+	@GetMapping("/todos")
+	public String getRecetas(Model model) {
+		return "testimonios";
+	}
 
 }
