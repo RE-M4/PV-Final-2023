a99276482dd81589db94025e86ab48bbbe83f222
DESKTOP-OQ78GJK\Elias <elias3847040@gmail.com>
2023-06-30 23:13:31 -0300

    Se agregar el pedido de codigo para paginas de Servicio
    -Se agrega un modal en el nav para pedido de codigo para redirigir a la
    pagina calcular_IMC
    
    -Se agrega un modal en el nav para pedido de codigo para redirigir a la
    pagina calcular_peso
    
    -Para ambos casos se agrego peticiones getmapping en el
    servicioController

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/ServicioController.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/controller/ServicioController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ar.edu.unju.fi.repository.IUsuarioRepository;
 
@@ -17,11 +18,27 @@ public class ServicioController {
 	private IUsuarioRepository usuarioRepository;
 	
 	@GetMapping("/imc")
-	public ModelAndView getCalculaIMC(@RequestParam(value="codigo") String codigo) {
+	public ModelAndView getCalculaIMC(@RequestParam(value="codigo") String codigo,RedirectAttributes redirectAttrs) {
 		System.out.println(codigo);
 		ModelAndView modelAndView = new ModelAndView("calcular_IMC");
 		if(usuarioRepository.findByCodigo(codigo) == null) {
-			modelAndView.setViewName("redirect:/#");
+			 redirectAttrs.addFlashAttribute("mensaje", "Codigo incorrecto");
+			
+			 modelAndView.setViewName("redirect:/index");
+		}
+		
+		
+		return modelAndView;
+	}
+	
+	@GetMapping("/peso_ideal")
+	public ModelAndView getPesoIdeal(@RequestParam(value="codigo") String codigo,RedirectAttributes redirectAttrs) {
+		System.out.println(codigo);
+		ModelAndView modelAndView = new ModelAndView("calcular_peso");
+		if(usuarioRepository.findByCodigo(codigo) == null) {
+			 redirectAttrs.addFlashAttribute("mensaje", "Codigo incorrecto");
+			
+			 modelAndView.setViewName("redirect:/index");
 		}
 		
 		
