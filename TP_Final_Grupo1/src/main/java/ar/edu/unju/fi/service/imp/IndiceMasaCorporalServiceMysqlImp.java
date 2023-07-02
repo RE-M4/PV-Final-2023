6ddd9faa85b6409b1bd627bd49b59e0722557956
Javier <Javier@DESKTOP-6AIS6L0>
2023-07-02 19:56:07 -0300

    Implementaciones en IndiceMasaCorporalServiceMysqlImp

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/service/imp/IndiceMasaCorporalServiceMysqlImp.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/service/imp/IndiceMasaCorporalServiceMysqlImp.java
@@ -1,5 +1,64 @@
 package ar.edu.unju.fi.service.imp;
 
-public class IndiceMasaCorporalServiceMysqlImp {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.entity.IndiceMasaCorporal;
+
+import ar.edu.unju.fi.repository.IIndiceMasaCorporalRepository;
+
+import ar.edu.unju.fi.service.IIndiceMasaCorporalService;
+
+@Service("IndiceMasaCorporalServiceMysqlImp")
+public class IndiceMasaCorporalServiceMysqlImp implements IIndiceMasaCorporalService{
+	
+	@Autowired
+    private IIndiceMasaCorporalRepository indiceMasaCorporalRepository;
+	
+	@Autowired
+	private IndiceMasaCorporal indiceMasaCorporal;
+
+	@Override
+	public IndiceMasaCorporal getImc() {
+		// TODO Auto-generated method stub
+		return indiceMasaCorporal;
+	}
+
+	@Override
+	public List<IndiceMasaCorporal> getListaImc() {
+		// TODO Auto-generated method stub
+		return (List<IndiceMasaCorporal>) indiceMasaCorporalRepository.findAll();
+	}
+
+	@Override
+	public void guardarImc(IndiceMasaCorporal indiceMasaCorporal) {
+		// TODO Auto-generated method stub
+		indiceMasaCorporalRepository.save(indiceMasaCorporal);
+		
+	}
+
+	@Override
+	public void modificarImc(IndiceMasaCorporal indiceMasaCorporal) {
+		// TODO Auto-generated method stub
+		indiceMasaCorporalRepository.save(indiceMasaCorporal);
+		
+	}
+
+	@Override
+	public IndiceMasaCorporal buscarImc(long id) {
+		// TODO Auto-generated method stub
+		return indiceMasaCorporalRepository.findById(id).get();
+	}
+
+	@Override
+	public void eliminarImc(IndiceMasaCorporal indiceMasaCorporal) {
+		// TODO Auto-generated method stub
+		indiceMasaCorporalRepository.delete(indiceMasaCorporal);
+		
+	}
+	
+	
 
 }
