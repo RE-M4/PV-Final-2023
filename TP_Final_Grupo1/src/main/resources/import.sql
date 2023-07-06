7793539c45f3e8036fd19cd06fb09d3468c366b0
Mauro Levi Rendón <88637963+RE-M4@users.noreply.github.com>
2023-07-06 11:39:57 -0300

    Actualización de "mostrar_recetas.html"
    -Se termina la estructura básica para mostrar recetas de cierta
    categoría.
    -Se agrega datos dentro de "import.sql".

--- TP_Final_Grupo1/src/main/resources/import.sql
+++ TP_Final_Grupo1/src/main/resources/import.sql
@@ -11,13 +11,19 @@ INSERT INTO ingredientes (nombre) values("Pollo")
 INSERT INTO ingredientes (nombre) values("Carne")
 
 INSERT INTO recetas (rec_categoria,rec_estado,rec_imagen,rec_nombre,rec_preparacion) values("Carne",1,"Imagen1","Carne con arroz","Preparacion1")
-INSERT INTO recetas (rec_categoria,rec_estado,rec_imagen,rec_nombre,rec_preparacion) values("Ensalada",0,"Imagen2","Ensalada mixta","Preparacion2")
+INSERT INTO recetas (rec_categoria,rec_estado,rec_imagen,rec_nombre,rec_preparacion) values("Ensalada",1,"Imagen2","Ensalada mixta","Preparacion2")
+INSERT INTO recetas (rec_categoria,rec_estado,rec_imagen,rec_nombre,rec_preparacion) values("Carne",1,"Imagen3","Carne con ensalada","Preparacion3")
 
 INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(1,1)
 INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(2,1)
 INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(3,1)
+
 INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(4,2)
 INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(5,2)
 INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(6,2)
+
+INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(3,3)
+INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(4,3)
+INSERT INTO recetas_lista_ingredientes(lista_ingredientes_ing_id,receta_rec_id) values(5,3)
 
 INSERT INTO testimonios (codigo, descripcion, usu_id) values("1", "Muy rico", "1")
