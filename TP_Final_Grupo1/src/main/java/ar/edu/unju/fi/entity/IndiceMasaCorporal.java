818ca3b6d2679cfd3ee674c3461be343383d9312
DESKTOP-OQ78GJK\Elias <elias3847040@gmail.com>
2023-07-07 00:10:45 -0300

    Se realiza el mapeo de relaciones entre usuario y IndiceMasaCorporal

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/entity/IndiceMasaCorporal.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/entity/IndiceMasaCorporal.java
@@ -1,14 +1,19 @@
 package ar.edu.unju.fi.entity;//es un PAQUETE  de organizacion de java.agrupa clases que contiene objetos para representar entidades.
 
+import java.time.LocalDate;
 import java.util.Date;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 @Component
 @Entity
@@ -22,11 +27,19 @@ public class IndiceMasaCorporal {
 	@Column(name="imc_fecha")
 	private Date fechaImc;
 	
-	/*@Column(name="imc_usuario")
-	private Usuario usuario;*/
+	@ManyToOne(cascade =  CascadeType.ALL,fetch = FetchType.EAGER)
+	@JoinColumn(name = "usu_id")
+	private Usuario usuario;
+	
 	
 	@Column(name="imc_estado")
 	private boolean estado;
+	
+	@Column(name="imc_peso")
+	private double peso;
+	
+	@Column(name="imc_corporal")
+	private String estadoCorporal;
 	
 	// --- GETTERS Y SETTERS ---
 	// Un getter es un método público que se utiliza para obtener el valor de una propiedad de la clase.
@@ -38,9 +51,9 @@ public class IndiceMasaCorporal {
 	public Date getFechaImc() {
 		return fechaImc;
 	}
-	/*public Usuario getUsuario() {
+	public Usuario getUsuario() {
 		return usuario;
-	}*/
+	}
 	public boolean getEstado() {
 		return estado;
 	}
@@ -50,28 +63,50 @@ public class IndiceMasaCorporal {
 	public void setFechaImc(Date fechaImc) {
 		this.fechaImc=fechaImc;
 	}
-	/*public void setUsuario(Usuario usuario) {
+	public void setUsuario(Usuario usuario) {
 		this.usuario=usuario;
-	}*/
+	}
 	public void setEstado(boolean estado) {
 		this.estado=estado;
 	}
+	public String getEstadoCorporal() {
+		return estadoCorporal;
+	}
+	public void setEstadoCorporal(String estadoCorporal) {
+		this.estadoCorporal = estadoCorporal;
+	}
+public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
 		
 	// --- CONSTRUCTOR ---
 	
-//Constructor con parámetros que tiene cuatro argumentos: Id - Fecha imc - Usuario - Estado.Estos se utilizan para inicializar los campos de la clase
-	public IndiceMasaCorporal(Long id, Date fechaImc, Usuario usuario, Boolean estado) {
+
+	//Constructor con parámetros que tiene cuatro argumentos: Id - Fecha imc - Usuario - Estado.Estos se utilizan para inicializar los campos de la clase
+	public IndiceMasaCorporal(Long id, Usuario usuario, Boolean estado) {
 		super();//se refiere a la llamada al constructor de la clase base (si existe).
 		this.id = id;
-		this.fechaImc = fechaImc;
+		this.fechaImc = new Date();
 		/*this.usuario = usuario;*/
 		this.estado = true;
 		//en este se asignan los valores a los campos anteriores. ESTE CREA UN OBJETO "IndiceMasaCorporal" y establece los valores de sus campos.
 	}
 	public IndiceMasaCorporal(){
 		this.estado=true;
+		this.fechaImc = new Date();
 	/*Este es un constructor sin parámetros. En este caso, se establece el campo estado en true. Este constructor puede ser útil 
 	 * cuando se desea crear un objeto IndiceMasaCorporal sin proporcionar valores específicos para sus campos en ese momento.*/	
 	}
+	@Override
+	public String toString() {
+		return "IndiceMasaCorporal [id=" + id + ", fechaImc=" + fechaImc + ", usuario=" + usuario + ", estado=" + estado
+				+ ", peso=" + peso + "]";
+	}
 
+	
+	
 }
