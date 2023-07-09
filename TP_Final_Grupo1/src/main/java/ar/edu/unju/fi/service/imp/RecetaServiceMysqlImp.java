1761592d1d63f59f8ec295af8a611ff3b9686e13
Mauro Levi Rendón <88637963+RE-M4@users.noreply.github.com>
2023-07-09 00:56:48 -0300

    Recuperación de código de la implementación Receta
    -Se recupera código funcional del método "eliminar" y
    "getListaRecetasByCategoria".

--- TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/service/imp/RecetaServiceMysqlImp.java
+++ TP_Final_Grupo1/src/main/java/ar/edu/unju/fi/service/imp/RecetaServiceMysqlImp.java
@@ -52,7 +52,7 @@ public class RecetaServiceMysqlImp implements IRecetaService{
 		// TODO Auto-generated method stub
 		return recetaRepository.findById(id).get();
 	}
-
+	
 	@Override
 	public List<Receta> getListaRecetasByCategoria(String categoria) {
 		// TODO Auto-generated method stub
